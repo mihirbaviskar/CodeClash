b9c371b69130f4e2df512b857bc17cde22dc0754
Mihir Baviskar <36090347+mihirbaviskar@users.noreply.github.com>
2023-07-29 02:32:33 -0700

    fixed compiler overflow error

--- frontend/src/components/ResponseDisplay.js
+++ frontend/src/components/ResponseDisplay.js
@@ -10,17 +10,16 @@ const ResponseDisplay = ({ response }) => {
     return null;
   }
 
-  let descriptionStyle = response.status.id === 3 ? {color:'green'} : {color:'red'};
-
+  let descriptionStyle = response.status.id === 3 ? {color:'#2cbb5d'} : {color:'#ef4643'};
   return (
     <div className="response-display">
-      {response.status.exit_code !==153 && response.status.description && <h3 style={descriptionStyle}>{response.status.description}</h3>}
-      {response.status.exit_code ===153 && <h3 style={descriptionStyle}>"Memory Limit Exceeded"</h3>}
+      {response.exit_code !==153 && response.status.description && <h3 className="submission-status" style={descriptionStyle}>{response.status.description}</h3>}
+      {response.exit_code === 153 && <h3 style={descriptionStyle}>Memory Limit Exceeded</h3>}
       {response.status.id === 6 && response.compile_output && <pre className="compiler-error-output">{response.compile_output}</pre>}
       {response.status.id === 11 && response.stdout && <><p className="output-label">stdout</p><pre className="response-output">{response.stdout}</pre></>}
       {response.status.id === 11 && response.stderr && <><p className="output-label">stderr</p><pre className="response-output">{response.stderr}</pre></>}
-      {response.status.id === 3 && response.wall_time && <p>Time {response.wall_time}</p>}
-      {response.status.id === 3 && response.memory && <p>Memory {response.memory}</p>}
+      {response.status.id === 3 && response.wall_time && <><p className="output-label">Time</p><pre className="response-output" >{response.wall_time}s</pre></>}
+      {response.status.id === 3 && response.memory && <><p className="output-label">Memory</p><pre className="response-output" >{response.memory}KB</pre></>}
     </div>
   );
   };
