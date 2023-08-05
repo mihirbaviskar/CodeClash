52bdf5d46cdd8735f627d6a972e9ba391831f4f7
Mihir Baviskar <36090347+mihirbaviskar@users.noreply.github.com>
2023-08-05 01:33:51 -0700

    fixed stopwatch

--- frontend/src/components/Stopwatch.js
+++ frontend/src/components/Stopwatch.js
@@ -1,13 +1,26 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { RoomContext } from "../context/RoomContext";
 import { UserContext } from "../context/UserContext";
 
 const Stopwatch = () => {
     const {room} = useContext(RoomContext);
+    const startTimeRef = useRef(null);
     const {user, elapsedTime, setElapsedTime} = useContext(UserContext);
+
+    useEffect(() => {
+        const d = new Date();
+        startTimeRef.current = (Math.floor(d.getTime()/1000));
+    }, []);
+
+
     useEffect(() => {
         let interval = setInterval(() => {
-            setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+            const d = new Date();
+            const new_val = Math.floor(d.getTime()/1000) - startTimeRef.current;
+            console.log('start: ' + startTimeRef.current);
+            console.log('curr: ' + Math.floor(d.getTime()/1000));
+            console.log('New val: ' + new_val);
+            setElapsedTime(new_val);
         }, 1000);
 
         // Cleanup the interval on component unmount
@@ -17,6 +30,8 @@ const Stopwatch = () => {
     useEffect(() => {
         console.log('Changing elapsed time');
         setElapsedTime(0);
+        const d = new Date();
+        startTimeRef.current = (Math.floor(d.getTime()/1000));
     }, [user.current_problem])
     
     const formatTime = (time) => {
